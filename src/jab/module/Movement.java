4ae348a3473f655b4f12e578806d34c30af69d04
Naufal Fikri Hakim <naufal.fikri11@ui.ac.id>
2025-03-14 14:44:39 +0700

    init code, movements, and targeting

---
+++ src/jab/module/Movement.java
@@ -0,0 +1,19 @@
+package jab.module;
+
+/**
+ * Movement
+ * 
+ * @author jab
+ */
+public  class  Movement  extends Part {
+	
+	public Module bot;
+
+	public Movement(Module bot) {
+		this.bot = bot;
+	}
+
+	public void move() {
+	}
+
+}
